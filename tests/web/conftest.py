a95e1c0382bb95c92b3b9c999a6a1ea1d769b8df
Murat Kubekov <murat.kubekov@maximumtest.ru>
2022-11-28 15:57:31 +0400

    modify conftest

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -1,11 +1,11 @@
 import os
 import pytest
+from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 from dotenv import load_dotenv
 from asos.utils.allure import attach
-
 
 
 # @pytest.fixture(scope='session', autouse=True)
@@ -20,29 +20,31 @@ from asos.utils.allure import attach
 #     browser.config.timeout = float(os.getenv('selene.timeout', '15'))
 #     browser.open('')
 
-DEFAULT_BROWSER_VERSION = "100.0"
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
-
-
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    load_dotenv()
+# DEFAULT_BROWSER_VERSION = "100.0"
+#
+#
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         '--browser_version',
+#         default='100.0'
+#     )
+#
+#
+# @pytest.fixture(scope='session', autouse=True)
+# def load_env():
+#     load_dotenv()
 
 
 @pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+def setup_browser():
+    browser_version = os.getenv('BROWSER_VERSION')
+    # browser_version = request.config.getoption('--browser_version')
+    # browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": browser_version,
+        "pageLoadStrategy": 'eager',
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -55,17 +57,18 @@ def setup_browser(request):
 
     driver = webdriver.Remote(
         # command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-        command_executor="http://selenoid:4444/wd/hub",
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser = Browser(Config(driver))
+    browser.config.driver = driver
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.timeout = 20.0
 
-    yield
+    yield browser
 
     attach.screenshot()
     attach.logs()
     attach.html_dump()
-    attach.video()
+    attach.video(browser)
     browser.close()
-
-
--- tests/web/test_asos.py
+++ tests/web/test_asos.py
@@ -2,10 +2,13 @@ import allure
 import pytest
 from selene import have
 from selene.support.shared import browser
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
 from asos.model.pages.catalog import Catalog
 from asos.model.pages.header import Main
 from asos.model.pages.product import Product
+from asos.utils.allure import attach
 
 main_page = Main()
 catalog = Catalog()
@@ -17,10 +20,13 @@ product = Product()
 @allure.tag('ui', 'nav', 'positive')
 @allure.label("owner", "Murat Kubekov")
 @allure.title('Open catalog page')
-@pytest.mark.parametrize('gender', ['men', 'women'])
-@pytest.mark.parametrize('product_type', ['boots', 'trainers'])
-def test_open_catalog(gender, product_type):
+# @pytest.mark.parametrize('gender', ['men', 'women'])
+# @pytest.mark.parametrize('product_type', ['boots', 'trainers'])
+@pytest.mark.parametrize('gender', ['men'])
+@pytest.mark.parametrize('product_type', ['boots'])
+def test_open_catalog(setup_browser, gender, product_type):
     with allure.step(f'Opening the {product_type} page'):
+        browser.open('https://asos.com')
         main_page \
             .check_gender(gender) \
             .select_category('shoes') \
@@ -31,96 +37,97 @@ def test_open_catalog(gender, product_type):
         catalog.product_cards().should(have.size_greater_than(0))
 
 
-@allure.feature('Filter')
-@allure.suite('Catalog page')
-@allure.tag('ui', 'filter', 'positive')
-@allure.label("owner", "Murat Kubekov")
-@allure.title('Select brand on catalog page')
-@pytest.mark.parametrize('brand', ['ASOS DESIGN', 'ASOS 4505'])
-def test_brand_selection(brand):
-    with allure.step('Оpening the catalog page'):
-        main_page \
-            .check_gender('men') \
-            .select_category('clothing') \
-            .select_subcategory('shirts')
 
-    with allure.step(f'Select brand {brand}'):
-        catalog \
-            .select_brand(brand) \
-            .product_cards().should(have.size_greater_than(0))
-
-
-@allure.feature('Search')
-@allure.suite('Main page')
-@allure.tag('ui', 'search', 'positive')
-@allure.label("owner", "Murat Kubekov")
-@allure.title('Search product product on main page')
-@pytest.mark.parametrize("value", ['shirts', 'shorts', 'socks'])
-def test_search_product(value):
-    with allure.step(f'Search product {value}'):
-        main_page.search(value)
-
-    with allure.step('Check the title and the availability of goods'):
-        catalog\
-            .search_banner().should(have.text(value.capitalize()))\
-            .product_cards().should(have.size_greater_than(0))
-
-
-@allure.feature('Save product')
-@allure.suite('Catalog page')
-@allure.tag('ui', 'save', 'positive')
-@allure.label("owner", "Murat Kubekov")
-@allure.title('Add to saved on the catalog page')
-def test_add_to_saved_on_the_catalog_page():
-    with allure.step('Search product'):
-        main_page.search('shirts')
-
-    with allure.step('Open product card'):
-        catalog.add_product_to_saved()
-
-    with allure.step('Check that the product is saved'):
-        main_page.move_to_saved()
-        browser.all('#saved-lists-app li').should(have.size(1))
-
-
-@allure.feature('Save product')
-@allure.suite('Product page')
-@allure.tag('ui', 'save', 'positive')
-@allure.label("owner", "Murat Kubekov")
-@allure.title('Add to saved on the product page')
-def test_add_product_to_saved():
-    with allure.step('Search product'):
-        main_page.search('shirts')
-
-    with allure.step('Open product card'):
-        catalog.select_product('')
-
-    with allure.step('Add product to saved'):
-        product \
-            .select_size('UK 8') \
-            .add_to_wishlist()
-
-    with allure.step('Check that the product is saved'):
-        main_page.move_to_saved()
-        browser.all('#saved-lists-app li').should(have.size(1))
-
-
-@allure.feature('Bag')
-@allure.suite('Product page')
-@allure.tag('ui', 'bag', 'positive')
-@allure.label("owner", "Murat Kubekov")
-@allure.title('Add to bag on the product page')
-def test_add_product_to_bag():
-    with allure.step('Search product'):
-        main_page.search('shirts')
-
-    with allure.step('Open product card'):
-        catalog.select_product('')
-
-    with allure.step('Add product to saved'):
-        product \
-            .select_size('UK 8') \
-            .add_to_bag()
-
-    with allure.step('Check that the product is saved'):
-        browser.all('#sminibag-dropdown li').should(have.size(1))
+# @allure.feature('Filter')
+# @allure.suite('Catalog page')
+# @allure.tag('ui', 'filter', 'positive')
+# @allure.label("owner", "Murat Kubekov")
+# @allure.title('Select brand on catalog page')
+# @pytest.mark.parametrize('brand', ['ASOS DESIGN', 'ASOS 4505'])
+# def test_brand_selection(brand):
+#     with allure.step('Оpening the catalog page'):
+#         main_page \
+#             .check_gender('men') \
+#             .select_category('clothing') \
+#             .select_subcategory('shirts')
+#
+#     with allure.step(f'Select brand {brand}'):
+#         catalog \
+#             .select_brand(brand) \
+#             .product_cards().should(have.size_greater_than(0))
+#
+#
+# @allure.feature('Search')
+# @allure.suite('Main page')
+# @allure.tag('ui', 'search', 'positive')
+# @allure.label("owner", "Murat Kubekov")
+# @allure.title('Search product product on main page')
+# @pytest.mark.parametrize("value", ['shirts', 'shorts', 'socks'])
+# def test_search_product(value):
+#     with allure.step(f'Search product {value}'):
+#         main_page.search(value)
+#
+#     with allure.step('Check the title and the availability of goods'):
+#         catalog\
+#             .search_banner().should(have.text(value.capitalize()))\
+#             .product_cards().should(have.size_greater_than(0))
+#
+#
+# @allure.feature('Save product')
+# @allure.suite('Catalog page')
+# @allure.tag('ui', 'save', 'positive')
+# @allure.label("owner", "Murat Kubekov")
+# @allure.title('Add to saved on the catalog page')
+# def test_add_to_saved_on_the_catalog_page():
+#     with allure.step('Search product'):
+#         main_page.search('shirts')
+#
+#     with allure.step('Open product card'):
+#         catalog.add_product_to_saved()
+#
+#     with allure.step('Check that the product is saved'):
+#         main_page.move_to_saved()
+#         browser.all('#saved-lists-app li').should(have.size(1))
+#
+#
+# @allure.feature('Save product')
+# @allure.suite('Product page')
+# @allure.tag('ui', 'save', 'positive')
+# @allure.label("owner", "Murat Kubekov")
+# @allure.title('Add to saved on the product page')
+# def test_add_product_to_saved():
+#     with allure.step('Search product'):
+#         main_page.search('shirts')
+#
+#     with allure.step('Open product card'):
+#         catalog.select_product('')
+#
+#     with allure.step('Add product to saved'):
+#         product \
+#             .select_size('UK 8') \
+#             .add_to_wishlist()
+#
+#     with allure.step('Check that the product is saved'):
+#         main_page.move_to_saved()
+#         browser.all('#saved-lists-app li').should(have.size(1))
+#
+#
+# @allure.feature('Bag')
+# @allure.suite('Product page')
+# @allure.tag('ui', 'bag', 'positive')
+# @allure.label("owner", "Murat Kubekov")
+# @allure.title('Add to bag on the product page')
+# def test_add_product_to_bag():
+#     with allure.step('Search product'):
+#         main_page.search('shirts')
+#
+#     with allure.step('Open product card'):
+#         catalog.select_product('')
+#
+#     with allure.step('Add product to saved'):
+#         product \
+#             .select_size('UK 8') \
+#             .add_to_bag()
+#
+#     with allure.step('Check that the product is saved'):
+#         browser.all('#sminibag-dropdown li').should(have.size(1))
